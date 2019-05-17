b1bf7357cfcf9e9236ed6b5be72d1daf5731082a
Antoniya Bocheva <abocheva65@googlemail.com>
2019-05-17 14:50:25 +0100

    more changes

--- server.js
+++ server.js
@@ -11,23 +11,23 @@ var app = express();
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true});
+/** this project needs a db !! **/
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 //creating the model with schema
 
-var UrlShortener = new mongoose.Schema ({
- "original_url": String,
- "short_url": String
+var urlShortener = new mongoose.Schema({
+    "original_url": String,
+    "short_url": String
 });
 
-var urlShortenerModel = mongoose.model("urlShortenerModel", UrlShortener);
+var urlShortenerModel = mongoose.model("urlShortenerModel", urlShortener);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
-app.use(bodyParser.urlencoded({extended: 'false'}));
+app.use(bodyParser.urlencoded({ extended: 'false' }));
 app.use(bodyParser.json());
 
 //use css
@@ -35,41 +35,79 @@ app.use(bodyParser.json());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 //get html
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+    res.json({ greeting: 'hello API' });
 });
 
 //post original url, but make sure it is valid first
-app.post('/api/shorturl/new', function(req, res){
+app.post('/api/shorturl/new', function (req, res) {
     var originalUrl = req.body.url;
     var domainName = originalUrl.replace(/(^\w+:|^)\/\//, "");
-    dns.lookup(domainName, function(error,url){
-    if(error){
-    res.json({"error": "invalid URL"});
-    } else{
-          var shortenedUrl = Math.floor(Math.random()*100000);
-          res.json({"original_url": originalUrl, "short_url": shortenedUrl});
-          }
+    dns.lookup(domainName, function (error, url) {
+        if (error) {
+            res.json({ "error": "invalid URL" });
+        } else {
+
+            CreateAndSaveShortUrl(originalUrl, function (newShortId) {
+                res.json({ "original_url": originalUrl, "short_url": newShortId });
+            });
+        }
     });
-  
-  //saving original url and short url in db in order to be able to get it after
-  
-  
+
 });
+
+function CreateAndSaveShortUrl(originalUrl, callback) {
+    // put code here
+  var shortenedUrl;
+urlShortenerModel.findOne({ "short_url": shortenedUrl}, function (err, item) {
+  if(item===true){
+        callback(item);
+  } else{
+  
+   shortenedUrl = Math.floor(Math.random()*100000);
+
+    //saving original url and short url in db in order to be able to get it after          
+    const newUrl = new urlShortenerModel({
+        "original_url": originalUrl,
+        "short_url": shortenedUrl
+    });
+
+    newUrl.save((err, data) => {
+    });
+
+}
+   callback(shortenedUrl);
+    });
+
+}
 
 
 //when the short url is visited redirect to original url
-app.get('/api/shorturl/(*)', function(req, res){
-//have to use findOne function here and then req. redirect()
-res.json({});
+app.get('/api/shorturl/:shortId', function (req, res) {
+    //have to use findOne function here and then req. redirect()
+    var shortId = req.params.shortId;
+    FindByShortId(shortId, function (item) {
+        console.log(item);
+        if (item) {
+            res.redirect(item.original_url);
+        } else {
+            res.redirect('/');
+        }
+    });
 });
 
+function FindByShortId(shortId, callback) {
+    urlShortenerModel.findOne({ "short_url": shortId }, function (err, item) {
+        callback(item);
+    });
+}
+
 app.listen(port, function () {
-  console.log('Node.js listening ...');
+    console.log('Node.js listening ...');
 });
