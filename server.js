74607e5a0afa81ad2288b37777e50fdcff83ac5d
Antoniya Bocheva <abocheva65@googlemail.com>
2019-05-20 08:39:58 +0100

    updates

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 //creating the model with schema
 
 var urlShortener = new mongoose.Schema({
-  "original_url": String,
-  "short_url": String
+    "original_url": String,
+    "short_url": String
 });
 
 var urlShortenerModel = mongoose.model("urlShortenerModel", urlShortener);
@@ -36,77 +36,78 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 //get html
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({ greeting: 'hello API' });
+    res.json({ greeting: 'hello API' });
 });
 
 //post original url, but make sure it is valid first
 app.post('/api/shorturl/new', function (req, res) {
-  var originalUrl = req.body.url;
-  var domainName = originalUrl.replace(/(^\w+:|^)\/\//, "");
-  dns.lookup(domainName, function (error, url) {
-    if (error) {
-      res.json({ "error": "invalid URL" });
-    } else {
+    var originalUrl = req.body.url;
+    var domainName = originalUrl.replace(/(^\w+:|^)\/\//, "");
+    dns.lookup(domainName, function (error, url) {
+        if (error) {
+            res.json({ "error": "invalid URL" });
+        } else {
 
-      CreateAndSaveShortUrl(originalUrl, function (newShortId) {
-        res.json({ "original_url": originalUrl, "short_url": newShortId });
-      });
-    }
-  });
+            CreateAndSaveShortUrl(originalUrl, function (newShortId) {
+                res.json({ "original_url": originalUrl, "short_url": newShortId });
+            });
+        }
+    });
 
 });
 
 function CreateAndSaveShortUrl(originalUrl, callback) {
-  // put code here
-  var shortenedUrl;
-  urlShortenerModel.findOne({ "short_url": shortenedUrl }, function (err, item) {
-    if (item === true) {
-      callback(item);
-    } else {
+    urlShortenerModel.findOne({ "original_url": originalUrl }, function (err, item) {
 
-      shortenedUrl = Math.floor(Math.random() * 100000);
+        if (item !== null) {
+            callback(item.short_url);
+        } else {
+            var shortId = GenerateUniqueShortId();
+            //saving original url and short url in db in order to be able to get it after          
+            const newUrl = new urlShortenerModel({
+                "original_url": originalUrl,
+                "short_url": shortId
+            });
 
-      //saving original url and short url in db in order to be able to get it after          
-      const newUrl = new urlShortenerModel({
-        "original_url": originalUrl,
-        "short_url": shortenedUrl
-      });
+            newUrl.save();
 
-      newUrl.save((err, data) => {
-      });
-
-    }
-    callback(shortenedUrl);
-  });
+            callback(shortId);
+        }
+    });
 }
 
 
 //when the short url is visited redirect to original url
 app.get('/api/shorturl/:shortId', function (req, res) {
-  //have to use findOne function here and then req. redirect()
-  var shortId = req.params.shortId;
-  FindByShortId(shortId, function (item) {
-    console.log(item);
-    if (item) {
-      res.redirect(item.original_url);
-    } else {
-      res.redirect('/');
-    }
-  });
+    //have to use findOne function here and then req. redirect()
+    var shortId = req.params.shortId;
+    FindByShortId(shortId, function (item) {
+        console.log(item);
+        if (item) {
+            res.redirect(item.original_url);
+        } else {
+            res.redirect('/');
+        }
+    });
 });
 
 function FindByShortId(shortId, callback) {
-  urlShortenerModel.findOne({ "short_url": shortId }, function (err, item) {
-    callback(item);
-  });
+    urlShortenerModel.findOne({ "short_url": shortId }, function (err, item) {
+        callback(item);
+    });
+}
+
+function GenerateUniqueShortId() {
+    var candidateId = Math.floor(Math.random() * 9999999999999999999);
+    return candidateId;
 }
 
 app.listen(port, function () {
-  console.log('Node.js listening ...');
+    console.log('Node.js listening ...');
 });
